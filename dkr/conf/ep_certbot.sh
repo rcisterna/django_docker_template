073f06574cc42fa5844aaff48792800ecf060d1e
Ricardo Cisterna <rcisterna@cadetech.cl>
2019-04-25 16:00:22 -0400

    certbot does not create fake SSL certificates

--- dkr/conf/ep_certbot.sh
+++ dkr/conf/ep_certbot.sh
@@ -7,30 +7,25 @@ if [ "$DDT_ENV" != "production" ]; then
 fi
 
 export LETSENCRYPT_DIR=/etc/letsencrypt
-export CA_DIR=${LETSENCRYPT_DIR}/live/${DDT_SSL_NAME}
 
-if [ ! -e "${LETSENCRYPT_DIR}/options-ssl-nginx.conf" ] || [ ! -e "${LETSENCRYPT_DIR}/ssl-dhparams.pem" ]; then
-	echo "### DDT: CERTBOT -- FAKE CA"
-
-	mkdir -p ${CA_DIR}
-
-	wget -O ${LETSENCRYPT_DIR}/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf
-	wget -O ${LETSENCRYPT_DIR}/ssl-dhparams.pem https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem
-
-	openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
-		-keyout "${CA_DIR}/privkey.pem" \
-		-out "${CA_DIR}/fullchain.pem" \
-		-subj "/CN=localhost"
-
-	sleep 3s
-	exit
-fi
-
+# Crea el certificado si no existe
 if [ ! -e "${LETSENCRYPT_DIR}/cntx_ssl_done" ]; then
-	echo "### DDT: CERTBOT -- ACME-CHALLENGE"
-	rm -rf ${CA_DIR} && \
+	echo "### DDT: CERTBOT -- NEW CERTIFICATE"
+	rm -rf ${LETSENCRYPT_DIR}/live/${DDT_SSL_NAME} && \
 	rm -rf ${LETSENCRYPT_DIR}/archive/${DDT_SSL_NAME} && \
 	rm -rf ${LETSENCRYPT_DIR}/renewal/${DDT_SSL_NAME}.conf
+
+	mkdir -p ${LETSENCRYPT_DIR}/live/${DDT_SSL_NAME}
+
+	if [ ! -e "${LETSENCRYPT_DIR}/options-ssl-nginx.conf" ]; then
+		wget -O ${LETSENCRYPT_DIR}/options-ssl-nginx.conf \
+			https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf
+	fi
+
+	if [ ! -e "${LETSENCRYPT_DIR}/ssl-dhparams.pem" ]; then
+		wget -O ${LETSENCRYPT_DIR}/ssl-dhparams.pem \
+			https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem
+	fi
 
     # Si se desean hacer pruebas, es necesario agregar argumento --staging
     # para no superar el rate limit semanal
@@ -39,14 +34,30 @@ if [ ! -e "${LETSENCRYPT_DIR}/cntx_ssl_done" ]; then
 		--domains ${DDT_DOMAINS// /,} --force-renewal
 
 	if [ $? -eq 0 ]; then
-		touch ${LETSENCRYPT_DIR}/cntx_ssl_done
+		echo ${DDT_DOMAINS} > ${LETSENCRYPT_DIR}/cntx_ssl_done
 		sleep 3s
-		echo "### DDT: CERTBOT -- ACME-CHALLENGE SUCCESS"
+		echo "### DDT: CERTBOT -- CERTIFICATE SUCCESS"
 	else
-		echo "### DDT: CERTBOT -- ACME-CHALLENGE FAILURE"
+		echo "### DDT: CERTBOT -- CERTIFICATE FAILURE"
 	fi
 	exit
 fi
 
-echo "### DDT: CERTBOT -- PRODUCTION"
+# Actualiza el certificado si se creó para dominios distintos
+if [[ $(< ${LETSENCRYPT_DIR}/cntx_ssl_done) != "$str" ]]; then
+	echo "### DDT: CERTBOT -- UPDATE CERTIFICATE"
+	certbot certonly --cert-name ${DDT_SSL_NAME} --domains ${DDT_DOMAINS// /,}
+
+	if [ $? -eq 0 ]; then
+		echo ${DDT_DOMAINS} > ${LETSENCRYPT_DIR}/cntx_ssl_done
+		sleep 3s
+		echo "### DDT: CERTBOT -- CERTIFICATE SUCCESS"
+	else
+		echo "### DDT: CERTBOT -- CERTIFICATE FAILURE"
+	fi
+	exit
+fi
+
+# Loop infinito para revisar renovación
+echo "### DDT: CERTBOT -- RENEW LOOP"
 trap exit TERM; while :; do certbot renew; sleep 12h & wait $!; done;
